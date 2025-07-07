34d23a94479f56591da5624649241864a04f628f
auto1302 <axtz7140@gmail.com>
2025-07-07 21:46:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function Gokfc() {
+    alert("Go to kfc");
+    window.location.href = "https://g.co/kgs/HxiN5VJ"
+}
+
+function Gopizza() {
+    alert("Go to pizza")
+    window.location.href = "https://g.co/kgs/9vWuYhy"
+}
+
+function Goseveneleven() {
+    alert("Go to seven-eleven")
+    window.location.href = "https://www.7eleven.co.th/"
+}
